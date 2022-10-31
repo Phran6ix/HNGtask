2f2fdc284f3004211ea4ba48b257a80f0998579a
Phran6ix <francisolatinwo@gmail.com>
2022-10-31 18:57:10 +0100

    Updatejson

--- route.js
+++ route.js
@@ -3,8 +3,6 @@ const output = require("./output.json");
 const router = Router();
 
 router.get("/gethnginfo", (req, res, next) => {
-  res.status(200).json({
-    output,
-  });
+  res.status(200).json(output);
 });
 module.exports = router;
